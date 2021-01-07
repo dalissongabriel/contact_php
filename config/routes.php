5f544dfb4751cc1fbe099cca47374945e4018db7
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-07 13:02:53 -0300

    feat: add route for persist contact

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,8 @@
 <?php
 
 use App\Netshowme\Infra\Controllers\ContactFormController;
+use App\Netshowme\Infra\Controllers\ContactPersistController;
 
-return ["/contato"=> ContactFormController::class];
+return [
+    "/contato"=> ContactFormController::class,
+    "/contato/enviar"=> ContactPersistController::class];
--- src/Infra/Controllers/ContactPersistController.php
+++ src/Infra/Controllers/ContactPersistController.php
@@ -4,14 +4,39 @@
 namespace App\Netshowme\Infra\Controllers;
 
 
+use \DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 class ContactPersistController implements RequestHandlerInterface
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+
+        $this->entityManager = $entityManager;
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $body = $request->getParsedBody();
+        $name = filter_var($body["name"], FILTER_SANITIZE_STRING);
+        $message = filter_var($body['message'],FILTER_SANITIZE_STRING);
+        $email = filter_var($body['email'], FILTER_VALIDATE_EMAIL);
+        $phone = filter_var($body['phone'], FILTER_SANITIZE_STRING);
+        $file = $body['file'];
+        $host = $_SERVER['REMOTE_ADDR'];
+        $createdAt = (new DateTimeImmutable())->format("Y-m-d H:i:s");
+
+        var_dump($name,$message,$email,$phone,$file,$host,$createdAt);
+        die();
+
 
     }
 }
