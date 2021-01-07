5d22bafa0707797420eccc583d8cb9fbac43982b
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-07 13:05:05 -0300

    feat: make contact entity

--- src/Domain/Entity/ContactEntity.php
+++ src/Domain/Entity/ContactEntity.php
@@ -7,4 +7,27 @@ namespace App\Netshowme\Domain\Entity;
 class ContactEntity
 {
 
+    private string $name;
+    private string $email;
+    private \DateTimeImmutable $createdAt;
+    private string $message;
+    private string $phone;
+    private string $file;
+
+    public function __construct(
+        string $name,
+        string $email,
+        \DateTimeImmutable $createdAt,
+        string $message,
+        string $phone,
+        string $file
+    )
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->createdAt = $createdAt;
+        $this->message = $message;
+        $this->phone = $phone;
+        $this->file = $file;
+    }
 }
