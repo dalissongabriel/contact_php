369ee0c369db2cbca2e07c5de627c247468ab427
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-08 18:25:47 -0300

    test: must ensure domain throws exception with invalid email

--- tests/Unit/Domain/Contact/ContactTest.php
+++ tests/Unit/Domain/Contact/ContactTest.php
@@ -5,6 +5,7 @@ namespace Tests\Netshowme\Unit\Domain\Contact;
 
 
 use App\Netshowme\Domain\Contact\Entity\Contact;
+use App\Netshowme\Domain\Share\Exceptions\InvalidEmailException;
 use Nyholm\Psr7\UploadedFile;
 use PHPUnit\Framework\TestCase;
 
@@ -23,4 +24,15 @@ class ContactTest extends TestCase
         $this->assertSame("test@test.com",(string) $contact->getEmail());
     }
 
+    public function testMustEnsureContactThrownExceptionWhenInvalidEmail()
+    {
+        $this->expectException(InvalidEmailException::class);
+        $name="test";
+        $email="test@";
+        $message="test";
+        $phone="(99) 999999999";
+        $host="127.0.0.1";
+
+        $contact = new Contact($name, $email, $message, $phone, $host);
+    }
 }
