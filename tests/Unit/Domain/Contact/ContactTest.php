b3ea403e21fd7dccee140a7b4e300710686d10f1
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-08 19:13:00 -0300

    test: must ensure domain throws exception with invalid phone

--- tests/Unit/Domain/Contact/ContactTest.php
+++ tests/Unit/Domain/Contact/ContactTest.php
@@ -6,6 +6,7 @@ namespace Tests\Netshowme\Unit\Domain\Contact;
 
 use App\Netshowme\Domain\Contact\Entity\Contact;
 use App\Netshowme\Domain\Share\Exceptions\InvalidEmailException;
+use App\Netshowme\Domain\Share\Exceptions\InvalidPhoneException;
 use Nyholm\Psr7\UploadedFile;
 use PHPUnit\Framework\TestCase;
 use function DI\string;
@@ -15,24 +16,39 @@ class ContactTest extends TestCase
     public function testMustEnsureContactHasRequiredFields()
     {
 
-        $name="test";
+        $name="test nanem";
         $email="test@test.com";
-        $message="test";
+        $message="test message";
         $phone="(99) 999999999";
         $host="127.0.0.1";
 
         $contact = new Contact($name, $email, $message, $phone, $host);
+
         $this->assertSame("test@test.com", (string) $contact->getEmail());
         $this->assertSame("(99) 999999999", (string) $contact->getPhone());
+        $this->stringContains("test message", (string) $contact->getFullMessage());
+        $this->stringContains("test nanem", (string) $contact->getFullMessage());
     }
 
-    public function testMustEnsureContactThrownExceptionWhenInvalidEmail()
+    public function testMustEnsureDomainThrownExceptionWhenInvalidEmail()
     {
         $this->expectException(InvalidEmailException::class);
         $name="test";
         $email="test@";
         $message="test";
         $phone="(99) 999999999";
+        $host="127.0.0.1";
+
+        $contact = new Contact($name, $email, $message, $phone, $host);
+    }
+
+    public function testMustEnsureDomainThrownExceptionWhenInvalidPhone()
+    {
+        $this->expectException(InvalidPhoneException::class);
+        $name="test";
+        $email="test@teste.com";
+        $message="test";
+        $phone="(99) aaaaaaaa";
         $host="127.0.0.1";
 
         $contact = new Contact($name, $email, $message, $phone, $host);
