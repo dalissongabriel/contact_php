355552eb3a7353605b212912aa40ebb078d34349
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-09 19:50:08 -0300

    feat: add animation in form grid

--- src/Views/Contact/ContactFormView.php
+++ src/Views/Contact/ContactFormView.php
@@ -24,16 +24,16 @@
 </header>
 <main>
     <section id="formulario-contato">
-        <div>
+        <div class="animeLeft">
             <h2>Formulário para contato</h2>
             <form class="form" action="/contato/enviar" method="post" enctype="multipart/form-data">
-                <label for="name">Nome completo:</label>
+                <label for="name">Nome completo: <span class="required-field">*</span></label>
                 <input tabindex="3" name="name" id="name"  type="text" placeholder="Informe o seu nome completo" required/>
-                <label for="email">E-mail:</label>
+                <label for="email">E-mail: <span class="required-field">*</span></label>
                 <input name="email" id="email" type="email" placeholder="Informe o seu e-mail mais utilizado" required>
-                <label for="phone">Telefone/Celular:</label>
+                <label for="phone">Telefone/Celular: <span class="required-field">*</span></label>
                 <input name="phone" id="phone" type="tel"  placeholder="Informe o seu telefone/celular para contato" required>
-                <label for="message">Mensagem:</label>
+                <label for="message">Mensagem: <span class="required-field">*</span></label>
                 <textarea name="message" id="message" placeholder="Informe a mensagem que deseja nos enviar" required></textarea>
                 <label for="file">Anexo:</label>
                 <input type="file"  id="file" name="file" accept=".doc, .docx, .odt, application/pdf, text/plain">
@@ -46,4 +46,12 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed dui eget quam venenatis aliquet non tempus mi. Aenean eu gravida enim. Nam tempus lacinia tortor nec bibendum. Duis ullamcorper bibendum orci, id aliquet ipsum suscipit vel. Cras faucibus elementum quam, eu convallis ex hendrerit ut. Vestibulum ut magna ultrices, efficitur urna nec, vulputate lacus. In quis hendrerit odio. Mauris vulputate aliquam malesuada. Cras bibendum purus lacus, sit amet finibus nisi volutpat sed. Nunc suscipit finibus ultricies. Morbi et nunc sed dolor ultrices tincidunt scelerisque a diam. Ut venenatis pellentesque dui, pharetra vulputate lorem semper sed. Quisque pretium massa nulla, ac ultrices massa accumsan sit amet. In id lorem et elit elementum egestas ac sit amet lectus. Aenean dignissim suscipit tellus, porta dignissim metus blandit vel.</p>
         <p>Nulla odio erat, tempor sed augue sit amet, molestie tincidunt erat. Morbi augue velit, placerat vitae posuere sit amet, laoreet quis libero. Aliquam vel turpis ornare, dignissim neque eu, eleifend quam. Sed quis ante sit amet nunc consectetur dapibus at id orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas id condimentum libero. Nullam dignissim, justo eu congue mattis, neque magna suscipit est, rhoncus maximus dui ante sed est. Donec sed pretium metus.</p>
     </section>
-<?php require_once __DIR__ . "/footerView.php" ?>
+</main>
+<footer class="footer">
+    <div class="wrapper">
+        <p>Desenvolvido com carinho por Alisson Gabriel, candidato para a vaga de desenvolvedor!</p>
+    </div>
+</footer>
+<script src="/assets/js/animations.js"></script>
+</body>
+</html>
--- src/Views/Contact/footerView.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer class="footer">
-        <div class="wrapper">
-            <p>Desenvolvido com carinho por Alisson Gabriel, candidato para a vaga de desenvolvedor!</p>
-        </div>
-    </footer>
-</body>
-</html>
